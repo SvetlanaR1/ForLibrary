fb01eac60e16e254c7a759bf611ac5fea5ff3ce1
РоманчиковаС <@410-TEACHER>
2019-12-14 11:20:13 +0600

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/GanreController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class GanreController : Controller
+    {
+        // GET: Ganre
+        public ActionResult Index()
+        {
+            using (Model1 db = new Model1())
+            {
+                var ganres = db.Ganres.OrderBy(g=>g.Name).ToList();
+            
+                return View(ganres);
+            }
+        }
+    }
+}
